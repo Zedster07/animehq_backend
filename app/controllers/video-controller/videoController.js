3c0f4bef3d4b42d86a441b5e4ca8cc0ee3189ea5
Houidi Nacer eddine <nacereddine.houidi@gmail.com>
2024-01-22 14:31:45 +0100

    limit download speed

--- app/controllers/video-controller/videoController.js
+++ app/controllers/video-controller/videoController.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const { customDecrypt } = require('../../utils');
 const config = require('../../config/config');
-const throttle = require('stream-throttle');
+const {Throttle} = require('stream-throttle');
 class VideoController {
 
 
@@ -54,7 +54,7 @@ class VideoController {
 
     static createThrottledStream(filePath, speedLimitMbps) {
         const readStream = fs.createReadStream(filePath);
-        const throttleStream = new throttle.Throttle({ rate: speedLimitMbps * 1024 * 1024 }); // Convert speed limit to bytes per second
+        const throttleStream = new Throttle({ rate: speedLimitMbps * 1024 * 1024 }); // Convert speed limit to bytes per second
         
         readStream.pipe(throttleStream);
         
@@ -130,56 +130,59 @@ class VideoController {
         const fileSize = fileStats.size;
         console.log(videoPath);
         const range = req.headers.range;
-        if (range) {
-            console.log("there is range");
-            const videoSize = fs.statSync(videoPath).size;
-            const CHUNK_SIZE = 10 ** 6;
-            const start = range ? Number(range.replace(/\D/g, "")) : 0;
-            const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-            const contentLength = end - start + 1;
+        const readStream = fs.createReadStream(videoPath);
+        readStream.pipe(new Throttle({rate: 10})).pipe(res);
+        
+        // if (range) {
+        //     console.log("there is range");
+        //     const videoSize = fs.statSync(videoPath).size;
+        //     const CHUNK_SIZE = 10 ** 6;
+        //     const start = range ? Number(range.replace(/\D/g, "")) : 0;
+        //     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+        //     const contentLength = end - start + 1;
 
-            if (start >= 0 && start <= videoSize - 1) { // Check if start is within valid range
-                const headers = {
-                    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-                    "Accept-Ranges": "bytes",
-                    "Content-Length": contentLength,
-                    "Content-Type": "video/mp4",
-                };
+        //     if (start >= 0 && start <= videoSize - 1) { // Check if start is within valid range
+        //         const headers = {
+        //             "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        //             "Accept-Ranges": "bytes",
+        //             "Content-Length": contentLength,
+        //             "Content-Type": "video/mp4",
+        //         };
 
-                res.writeHead(206, headers);
+        //         res.writeHead(206, headers);
 
-                // Create a throttled stream with the specified speed limit (40 Mbps)
-                const throttledStream = VideoController.createThrottledStream(videoPath, 10);
+        //         // Create a throttled stream with the specified speed limit (40 Mbps)
+        //         const throttledStream = VideoController.createThrottledStream(videoPath, 10);
 
-                // Handle errors if necessary
-                throttledStream.on('error', (err) => {
-                    console.error(err);
-                    res.status(500).send('Internal Server Error');
-                });
+        //         // Handle errors if necessary
+        //         throttledStream.on('error', (err) => {
+        //             console.error(err);
+        //             res.status(500).send('Internal Server Error');
+        //         });
 
-                // Pipe the throttled stream with the specified range to the response
-                throttledStream.pipe(res, { start, end });
-            } else {
-                res.status(416).send('Requested Range Not Satisfiable'); // 416 Range Not Satisfiable
-            }
-        } else {
-            console.log("NO RANGE");
-            res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
-            res.setHeader('Content-Type', 'application/octet-stream');
-            res.setHeader('Content-Length', fileSize.toString());
+        //         // Pipe the throttled stream with the specified range to the response
+        //         throttledStream.pipe(res, { start, end });
+        //     } else {
+        //         res.status(416).send('Requested Range Not Satisfiable'); // 416 Range Not Satisfiable
+        //     }
+        // } else {
+        //     console.log("NO RANGE");
+        //     res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
+        //     res.setHeader('Content-Type', 'application/octet-stream');
+        //     res.setHeader('Content-Length', fileSize.toString());
 
-            // Create a throttled stream with the specified speed limit (40 Mbps)
-            const throttledStream = VideoController.createThrottledStream(videoPath, 10);
+        //     // Create a throttled stream with the specified speed limit (40 Mbps)
+        //     const throttledStream = VideoController.createThrottledStream(videoPath, 10);
 
-            // Handle errors if necessary
-            throttledStream.on('error', (err) => {
-                console.error(err);
-                res.status(500).send('Internal Server Error');
-            });
+        //     // Handle errors if necessary
+        //     throttledStream.on('error', (err) => {
+        //         console.error(err);
+        //         res.status(500).send('Internal Server Error');
+        //     });
 
-            // Pipe the throttled stream to the response
-            throttledStream.pipe(res);
-        }
+        //     // Pipe the throttled stream to the response
+        //     throttledStream.pipe(res);
+        // }
     }
 }
 
